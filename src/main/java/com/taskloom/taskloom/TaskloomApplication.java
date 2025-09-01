45547a1575091a981df5434e10c2aeabf34bee3c
alper <alpertopraktepe46@gmail.com>
2025-09-01 10:04:06 +0300

    first commit

---
+++ src/main/java/com/taskloom/taskloom/TaskloomApplication.java
@@ -0,0 +1,13 @@
+package com.taskloom.taskloom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskloomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskloomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taskloom/taskloom/TaskloomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskloom.taskloom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskloomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
